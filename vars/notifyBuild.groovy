a72a8a42d067bc46657c351c3b3f49da501fae26
Phuoc Truong <phuoctruong@alax.io>
2019-01-08 15:51:47 +0700

    try to fix

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -7,6 +7,7 @@ def call(String buildStatus = 'STARTED', String env, String version = '0.0.0.0',
     def color
     def defaultUrl = 'alax.io'
     def frontEndUrl= ''
+    def msg
 
     if (frontendApp != 'None'){
         switch(env) {
@@ -16,40 +17,41 @@ def call(String buildStatus = 'STARTED', String env, String version = '0.0.0.0',
                 } else {
                     frontEndUrl = 'https://devlopment-admin.' + url
                 }
-                break;
             case test:
                 if (frontendApp == 'publisher') {
                     frontEndUrl = 'https://test-store.' + url
                 } else {
                     frontEndUrl = 'https://test-admin.' + url
                 }
-                break;
             case staging:
                 if (frontendApp == 'publisher') {
                     frontEndUrl = 'https://staging-store.' + url
                 } else {
                     frontEndUrl = 'https://staging-admin.' + url
                 }
-                break;
             case prod:
                 if (frontendApp == 'publisher') {
                     frontEndUrl = 'https://store.' + url
                 } else {
                     frontEndUrl = 'https://admin.' + url
                 }
-                break;
         }
     }
 
     if (buildStatus == 'STARTED') {
         color = '#D4DADF'
+        msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\n* Version:${version}\n* Commit Message:\n${gitMessage}"
     } else if (buildStatus == 'SUCCESS') {
         color = '#00FF00'
     } else {
         color = '#FF0000'
     }
     
-    def msg = "${buildStatus}: ${env.JOB_NAME} #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\n* Version:${version}\n* Commit Message:\n${gitMessage}"
+    if (frontendApp == 'None'){
+        msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\n* Version:${version}\n* Commit Message:\n${gitMessage}"
+    } else {
+        msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}\n* Version:${version}\n* Deployed at: ${frontEndUrl}* Commit Message:\n${gitMessage}"
+    }
     
     slackSend(color: color, message: msg)
 }
