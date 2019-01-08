f996bf6cc2e4ce68c972a26f013eabe3210809aa
Phuoc Truong <truonghongphuoc@gmail.com>
2019-01-08 10:40:15 +0700

    Fix

--- vars/notifyBuild.groovy
+++ vars/notifyBuild.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
-def notifyBuild(String buildStatus = 'STARTED') {
+def call(String buildStatus = 'STARTED') {
   // build status of null means successful
   buildStatus =  buildStatus ?: 'SUCCESSFUL'
 
@@ -17,5 +17,4 @@ def notifyBuild(String buildStatus = 'STARTED') {
     def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
 
     slackSend(color: color, message: msg)
-
 }
