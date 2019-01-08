7ddbeb688824f3a14b1f91fbe2617ec573405a9b
Phuoc Truong <truonghongphuoc@gmail.com>
2019-01-08 10:15:54 +0700

    Add README and slackNotifier

---
+++ slackNotifier.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+
+def notifyBuild(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus =  buildStatus ?: 'SUCCESSFUL'
+
+    def color
+
+    if (buildStatus == 'STARTED') {
+        color = '#D4DADF'
+    } else if (buildStatus == 'SUCCESS') {
+        color = '#00FF00'
+    } else {
+        color = '#FF0000'
+    }
+
+    def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+    slackSend(color: color, message: msg)
+
+}
